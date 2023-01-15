901bd29e6df48eeb133ffe09bac0337c66b53a40
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-01-15 18:45:40 -0500

    add forcedTheme

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <Layout>
-        <Component {...pageProps} />
+        <Component forcedTheme="dark" {...pageProps} />
       </Layout>
     </ThemeProvider>
   );
