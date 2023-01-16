ddbb4046d05c224ff49568be6a7f844a000103ed
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-01-15 19:12:10 -0500

    overflow x hidden

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,7 +3,7 @@ import Header from "../Header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col p-5 overflow-y-hidden">
+    <div className="flex flex-col p-5 overflow-x-hidden">
       <Header />
       <main>{children}</main>
     </div>
