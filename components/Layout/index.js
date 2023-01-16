927cae615395eb9205bfc68a7917cd4e34e52fb3
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-01-15 19:09:37 -0500

    h-screen w-screen

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,7 +3,7 @@ import Header from "../Header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col p-5">
+    <div className="flex flex-col p-5 h-screen w-screen">
       <Header />
       <main>{children}</main>
     </div>
