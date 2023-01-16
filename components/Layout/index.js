93bc83f7338969abe63078ba48f6e6edbd423ad2
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-01-15 19:13:43 -0500

    update

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,7 +3,7 @@ import Header from "../Header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col p-5 overflow-x-hidden">
+    <div className="flex flex-col p-5">
       <Header />
       <main>{children}</main>
     </div>
