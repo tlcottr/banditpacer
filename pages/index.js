215178063ada048fbdf04135076ff0fed23415b0
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-02-02 13:12:47 -0500

    ENTER DISTANCE, type = "number"

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
           <input
             className="px-3 py-3 uppercase font-GroteskRegular text-2xl focus:border-black focus:ring-0 bg-transparent rounded-xl w-full text-black"
             placeholder="Enter Distance"
+            type="number"
           ></input>
           <UnitToggle />
         </div>
