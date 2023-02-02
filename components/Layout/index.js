b2cd6659443b68a6305067d031a9604682361047
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-02-01 22:19:05 -0500

    update

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,10 +3,10 @@ import Header from "../Header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col p-5">
+    <>
       <Header />
       <main>{children}</main>
-    </div>
+    </>
   );
 };
 
