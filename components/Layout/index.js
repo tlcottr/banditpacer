81844bb9fc68dcf42faa64d509b67ec75bccfafd
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-02-19 15:42:19 -0500

    updated

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,7 +3,7 @@ import Header from "../Header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="p-5 relative md:max-w-lg md:m-auto">
+    <div className="p-5 fixed inset-0 md:max-w-lg md:m-auto">
       <main>
         <Header />
         {children}
