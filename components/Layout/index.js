78f0126e697221fb87ac88c69a19b572606f53fd
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-07-01 17:50:35 -0400

    update

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -4,7 +4,6 @@ import Header from "../Header";
 const Layout = ({ children }) => {
   return (
     <div className="px-5 pt-[2vh] md:max-w-lg md:m-auto">
-      <Header />
       <main>{children}</main>
     </div>
   );
